6c195422f45693eea1028462ca3502da5ba63e97
Alex Spinnler <SpinnleA@advisory.com>
2014-04-07 23:59:39 -0500

    comment out broken as_json

--- model/city.py
+++ model/city.py
@@ -4,6 +4,7 @@ class City(object):
     def __init__(self, name, locations):
         self.name = name
         self.locations = locations
-        
+"""        
     def as_json(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+"""
