68d1358385c3b6e499c30d0cc9a128bdb27aee23
Alex Spinnler <SpinnleA@advisory.com>
2014-04-08 01:17:21 -0500

    allow optional url for location model

--- model/location.py
+++ model/location.py
@@ -1,5 +1,5 @@
 class Location(object):
-    def __init__(self, name, flavor_board):
+    def __init__(self, name, flavor_board, url = None):
         self.name = name
-        
         self.flavor_board = flavor_board
+        self.url = url
